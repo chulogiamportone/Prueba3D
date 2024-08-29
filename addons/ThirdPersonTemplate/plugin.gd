5dcd788d6667fc47bfc1241464506a5d74e30e4f
ChuchuGiampo <chulogiamportone@gmail.com>
2024-08-29 16:58:48 -0300

    v2

--- addons/ThirdPersonTemplate/plugin.gd
+++
@@ -1,7 +0,0 @@
-@tool
-extends EditorPlugin
-
-func _enter_tree():
-	add_custom_type("ThirdPersonController", "CharacterBody3D", preload("Character/Scripts/CharacterController.gd"), preload("icon.svg"))
-func _exit_tree():
-	remove_custom_type("ThirdPersonController")
